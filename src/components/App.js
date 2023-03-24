a9f694c89c93b44d9015bb35b8b680a2c5a87ce6
kratos2377 <shobityadav23@gmail.com>
2023-03-25 01:36:09 +0530

    Compnents and new token functionality added

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React , {Component} from "react";
+import Navbar from './Navbar'
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+          <div className="text-center">
+                    <h>Hello World</h>
+            </div>
+            <Navbar/>
+            </div>
+  
+        )
+    }
+}
+
+
+export default App;
--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -8,8 +8,52 @@ contract DecentralBank {
     Tether public tether;
     RWD public rwd;
 
+    address[] public stakers;
+
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool)  public isStaking;
+
     constructor(RWD _rwd , Tether _tether) public  {
+        owner = msg.sender;
 rwd = _rwd;
 tether = _tether;
     }
+
+        function depositTokens(uint amount) public {
+            require( amount > 0);
+            tether.transferFrom(msg.sender , address(this) , amount);
+
+            stakingBalance[msg.sender] += amount;
+            if(!hasStaked[msg.sender]){
+                stakers.push(msg.sender);
+            }
+
+            isStaking[msg.sender] = true;
+            hasStaked[msg.sender] = true;
+        }
+
+        function unstakeTokens() public {
+            uint balance = stakingBalance[msg.sender];
+            require(balance > 0);
+
+            tether.transfer(msg.sender , balance);
+
+            stakingBalance[msg.sender] = 0;
+
+            isStaking[msg.sender] = false;
+        }
+        function issueTokens() public {
+                require(owner == msg.sender);
+
+                for(uint i = 0 ; i<stakers.length; i++) {
+                    address recipient = stakers[i];
+                    uint balance = stakingBalance[recipient];
+                 if(balance > 0){
+                       rwd.transfer(recipient , balance);
+                 }
+
+                }
+        }
+
 }
